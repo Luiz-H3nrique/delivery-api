70e4b8baf2ccbfb11d1755aa90e5ad9d106fb0a0
Luiz-H3nrique <luizsantos894@gmail.ccom>
2025-07-14 09:23:53 -0300

    feature : ajustes no controller

--- src/main/java/com/deliverytech/delivery_api/controller/ClienteController.java
+++ src/main/java/com/deliverytech/delivery_api/controller/ClienteController.java
@@ -45,7 +45,6 @@ public class ClienteController {
         return ResponseEntity.ok(atualizado);
     }
 
-
     @PatchMapping("/{id}/ativo")
     public ResponseEntity<Void> alternarStatusAtivo(@PathVariable Long id) {
         clienteService.ativarDesativar(id);
